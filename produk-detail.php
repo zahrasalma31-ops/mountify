aaf2e795934426df485aa3b948afda62f8dce736
almas <almasikramina05@gmail.com>
2025-11-20 13:31:36 +0700

    detail produk

--- produk-detail.php
+++ produk-detail.php
@@ -1,11 +1,30 @@
 <?php
-    require "koneksi.php";
+require "koneksi.php";
 
-    $nama = htmlspecialchars($_GET['nama']);
-    $queryProduk = mysqli_query($con, "SELECT * FROM produk WHERE nama='$nama'");
-    $produk = mysqli_fetch_array($queryProduk);
+// Pastikan parameter id ada
+if (!isset($_GET['id'])) {
+    header("Location: produk.php");
+    exit();
+}
 
-    $queryProdukTerkait = mysqli_query($con, "SELECT * FROM produk WHERE kategori_id='$produk[kategori_id]' LIMIT 4");
+// Ambil data produk berdasarkan ID
+$id = intval($_GET['id']);
+$queryProduk = mysqli_query($con, "SELECT * FROM produk WHERE id = $id");
+$produk = mysqli_fetch_assoc($queryProduk);
+
+// Jika produk tidak ditemukan
+if (!$produk) {
+    echo "<h1>Produk tidak ditemukan</h1>";
+    exit();
+}
+
+// Produk terkait berdasarkan kategori_id
+$queryProdukTerkait = mysqli_query(
+    $con,
+    "SELECT * FROM produk 
+     WHERE kategori_id = {$produk['kategori_id']} AND id != $id 
+     LIMIT 4"
+);
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +37,7 @@
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="fontawesome/css/all.min.css">
   <link rel="stylesheet" href="css/style.css">
+
   <style>
     .booking-btn {
       display: inline-block;
@@ -31,7 +51,6 @@
       text-decoration: none;
       transition: background-color 0.3s ease;
     }
-
     .booking-btn:hover {
       background-color: #355a50;
       color: #fff;
@@ -39,62 +58,72 @@
     }
   </style>
 </head>
+
 <body>
-  <?php require "navbar.php"; ?>
+<?php require "navbar.php"; ?>
 
-  <!-- detail produk -->
-  <div class="container-fluid py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-5 mb-5">
-          <img src="image/<?php echo $produk['foto']; ?>" class="w-100" alt="">
-        </div>
+<!-- Detail Produk -->
+<div class="container-fluid py-5">
+  <div class="container">
+    <div class="row">
 
-        <div class="col-lg-6 offset-lg-1">
-          <h1 class="product-title"><?php echo $produk['nama']; ?></h1>
-          <p class="fs-5" style="text-align: justify;">
-            <?php echo nl2br ($produk['detail']); ?>
-          </p>
-          <p class="text-harga">
-            Rp <?php echo $produk['harga']; ?>
-          </p>
-          <p class="fs-5">
-            Status Ketersediaan:
-            <strong><?php echo $produk['ketersediaan_stok']; ?></strong>
-          </p>
+      <div class="col-lg-5 mb-5">
+        <img src="image/<?= $produk['foto']; ?>" class="w-100" alt="">
+      </div>
 
-          <!-- Tombol Booking -->
-          <a 
-            href="https://wa.me/628988712806?text=Hai%20saya%20ingin%20booking%20produk%20<?php echo urlencode($produk['nama']); ?>" 
-            target="_blank"
-            class="booking-btn">
-            Booking
+      <div class="col-lg-6 offset-lg-1">
+        <h1 class="product-title"><?= $produk['nama']; ?></h1>
+
+        <p class="fs-5" style="text-align: justify;">
+          <?= nl2br($produk['detail']); ?>
+        </p>
+
+        <p class="text-harga">
+          Rp <?= number_format($produk['harga'], 0, ',', '.'); ?>
+        </p>
+
+        <p class="fs-5">
+          Status Ketersediaan: <strong><?= $produk['ketersediaan_stok']; ?></strong>
+        </p>
+
+        <!-- Tombol booking -->
+        <a 
+          href="https://wa.me/628988712806?text=Hai%20saya%20ingin%20booking%20produk%20<?= urlencode($produk['nama']); ?>" 
+          target="_blank"
+          class="booking-btn">
+          Booking
+        </a>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Produk Terkait -->
+<div class="container-fluid py-5 warna2">
+  <div class="container">
+    <h2 class="text-center text-white mb-5">Produk Terkait</h2>
+
+    <div class="row">
+      <?php while ($data = mysqli_fetch_assoc($queryProdukTerkait)) { ?>
+        <div class="col-md-6 col-lg-3 mb-3">
+          <a href="produk-detail.php?id=<?= $data['id']; ?>">
+            <img src="image/<?= $data['foto']; ?>" 
+                 class="img-fluid img-thumbnail produk-terkait-image" 
+                 alt="">
           </a>
         </div>
-      </div>
+      <?php } ?>
     </div>
-  </div>
 
-  <!-- produk terkait -->
-  <div class="container-fluid py-5 warna2">
-    <div class="container">
-      <h2 class="text-center text-white mb-5">Produk Terkait</h2>
-      <div class="row">
-        <?php while ($data = mysqli_fetch_array($queryProdukTerkait)) { ?>
-          <div class="col-md-6 col-lg-3 mb-3">
-            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>">
-              <img src="image/<?php echo $data['foto']; ?>" class="img-fluid img-thumbnail produk-terkait-image" alt="">
-            </a>
-          </div>
-        <?php } ?>
-      </div>
-    </div>
   </div>
+</div>
 
-  <!-- footer -->
-  <?php require "footer.php"; ?>
-  
-  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="fontawesome/js/all.min.js"></script>
+
+<?php require "footer.php"; ?>
+
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="fontawesome/js/all.min.js"></script>
+
 </body>
 </html>
